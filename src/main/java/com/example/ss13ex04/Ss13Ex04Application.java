a9dfc8be11bf5ec187c4aed473a749392be1ba07
minh2307 <minh23923@gmail.com>
2026-04-22 20:10:32 +0700

    done

---
+++ src/main/java/com/example/ss13ex04/Ss13Ex04Application.java
@@ -0,0 +1,13 @@
+package com.example.ss13ex04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss13Ex04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss13Ex04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss13ex04/Ss13Ex04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss13ex04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss13Ex04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
